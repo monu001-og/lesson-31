bb91f0a24641cf5df6d7d29d81ad254eea4252e2
monu001-og <olatudeogbe@gmail.com>
2024-10-29 07:05:53 +0100

    stringop.py

---
+++ stringop.py
@@ -0,0 +1,15 @@
+first_name="Codingal"
+last_name="Educations"
+full_name=first_name + last_name
+example="Haa"*5
+
+print("First Name:",first_name)
+print("Last Name:",last_name)
+print("Full Name:",full_name)
+print("String Multiplied S times gives this result:",example)
+
+word="Coding"
+print("Length of String:",len(word))
+print("First letter of string:",word[0])
+print("Last Letter of String:",word[5])
+print("String Sliced:",word[0:3])
