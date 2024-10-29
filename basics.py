fcf69dda158907b3c22a26db47f5f46719d77826
monu001-og <olatudeogbe@gmail.com>
2024-10-29 06:52:17 +0100

    basics.py

---
+++ basics.py
@@ -0,0 +1,21 @@
+name ="Monu"
+age =17
+is_student = False
+weight = 68.4
+
+print("Name :"  ,name)
+print("Dta type of name is",type(name))
+print("Age :" ,age)
+print("Data type of Age is",type(age))
+print("is_student: "  ,is_student)
+print("Data type of is_student is"  ,type(is_student))
+print("Weight:"  ,weight)
+print("Data Type of Weight is" ,type(weight))
+
+print("\n After Type Casting...")
+age = str(age)
+print(age)
+print("Data Type of Age is",type(age))
+weight=int(weight)
+print(weight)
+print("Data type of weight is",type(weight))
